03d69f3149d0d42679e26d4c9cf27953bb1f347f
Seokho Jeong <kreel9402@gmail.com>
2023-08-01 15:29:54 +0900

    Initial Commit

---
+++ MyScrumdinger/MyScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyScrumdingerApp.swift
+//  MyScrumdinger
+//
+//  Created by 정유진 on 2023/08/01.
+//
+
+import SwiftUI
+
+@main
+struct MyScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
